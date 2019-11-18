8b4a5d0f95d4491f6e6913ca3ab5d6b375dfdc69
Jmarti24 <53451920+Jmarti24@users.noreply.github.com>
2019-11-18 08:24:09 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+
+$('.minus-btn').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+
+    if (value > 1) {
+        value = value - 1;
+    } else {
+        value = 0;
+    }
+
+    $input.val(value);
+
+});
+
+$('.plus-btn').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+
+    if (value < 100) {
+        value = value + 1;
+    } else {
+        value =100;
+    }
+
+    $input.val(value);
+});
+
+$('.delete-btn').on('click', function() {
+    $(this).remove('.image, .quantity, .description, .total-price');
+});
+
+$('.like-btn').on('click', function() {
+    $(this).toggleClass('is-active');
+});
+
+
+
+/*$('.delete-btn').on('click', function() {
+.remove('item');
+});*/
+
